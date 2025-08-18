98930c203eca78d8d3ea4323cdb5128c4ca63d74
khoihoanglsr <khoi280986@gmail.com>
2025-08-18 11:32:12 +0700

    Add code for GoFa robot

---
+++ Gofa_caffe/User Files/Project1/Virtual Controllers/CRB15000_5_95/internal/opt_l0.cmd
@@ -0,0 +1,4 @@
+
+include -path "C:/Users/DELL/AppData/Local/ABB/RobotWare/RobotControl_7.18.1/options/vnet/virtualInc_opt.cmd"
+
+include -path "C:/Users/DELL/AppData/Local/ABB/RobotWare/RobotControl_7.18.1/options/sc/ScInc_opt.cmd"
