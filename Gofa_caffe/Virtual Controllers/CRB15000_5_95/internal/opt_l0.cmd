b4595e2ba9a37b36eea528f9e1401e916d059cc0
thien_engi <30584644+buithanhthien@users.noreply.github.com>
2025-08-20 15:58:06 +0700

    updated 20_8_2025 3_57_PM

--- Gofa_caffe/Virtual Controllers/CRB15000_5_95/internal/opt_l0.cmd
+++ Gofa_caffe/Virtual Controllers/CRB15000_5_95/internal/opt_l0.cmd
@@ -1,4 +1,4 @@
 
-include -path "C:/Users/DELL/AppData/Local/ABB/RobotWare/RobotControl_7.18.1/options/vnet/virtualInc_opt.cmd"
+include -path "C:/Users/Thanh Thien/AppData/Local/ABB/RobotWare/RobotControl_7.18.0/options/vnet/virtualInc_opt.cmd"
 
-include -path "C:/Users/DELL/AppData/Local/ABB/RobotWare/RobotControl_7.18.1/options/sc/ScInc_opt.cmd"
+include -path "C:/Users/Thanh Thien/AppData/Local/ABB/RobotWare/RobotControl_7.18.0/options/sc/ScInc_opt.cmd"
